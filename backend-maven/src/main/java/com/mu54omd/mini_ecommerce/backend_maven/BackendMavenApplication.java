7c5b4f35a955fdd5143141359fe633aa8337e6fd
mu54omd <mu54omd@vivaldi.net>
2025-10-17 09:29:33 +0330

    feat(backend): initial Spring Boot backend + Git Setup

---
+++ backend-maven/src/main/java/com/mu54omd/mini_ecommerce/backend_maven/BackendMavenApplication.java
@@ -0,0 +1,13 @@
+package com.mu54omd.mini_ecommerce.backend_maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMavenApplication.class, args);
+	}
+
+}
---
+++ backend-maven/src/test/java/com/mu54omd/mini_ecommerce/backend_maven/BackendMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mu54omd.mini_ecommerce.backend_maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
