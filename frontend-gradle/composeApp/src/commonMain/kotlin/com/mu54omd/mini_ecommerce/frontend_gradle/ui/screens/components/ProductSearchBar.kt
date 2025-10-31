e35f2a29494b49533ce33530009bc2fb75195445
mu54omd <mu54omd@vivaldi.net>
2025-10-31 16:47:20 +0330

    feat: add material theme to the app for light and dark mode

---
+++ frontend-gradle/composeApp/src/commonMain/kotlin/com/mu54omd/mini_ecommerce/frontend_gradle/ui/screens/components/ProductSearchBar.kt
@@ -0,0 +1,2 @@
+package com.mu54omd.mini_ecommerce.frontend_gradle.ui.screens.components
+
---
+++ frontend-gradle/composeApp/src/commonMain/kotlin/com/mu54omd/mini_ecommerce/frontend_gradle/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.mu54omd.mini_ecommerce.frontend_gradle.ui.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
